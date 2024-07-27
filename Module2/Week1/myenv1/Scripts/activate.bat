0d5b1702e748ff31ead86083d5dd3c3795c5a336
Tag Nguyen <huyqthp03@gmail.com>
2024-07-27 16:21:08 +0700

    fix

--- Module2/Week1/myenv1/Scripts/activate.bat
+++ Module2/Week1/myenv1/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ASUS\Documents\AI\AIO-Exercise\myenv1
+set VIRTUAL_ENV=C:\Users\ASUS\Documents\AI\AIO-Exercise\Module2\Week1\myenv1
 
 if not defined PROMPT set PROMPT=$P$G
 
